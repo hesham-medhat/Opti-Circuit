c26132cbd8656e95c7794a36069171f98cf5bb62
EngHesham <heshammedhat5@gmail.com>
2017-05-03 10:46:56 +0200

    Add IEssentialPrimeImplicants interface

--- src/organizingCode/IEssentialPrimeImplicants.java
+++ src/organizingCode/IEssentialPrimeImplicants.java
@@ -1,15 +1,29 @@
 package organizingCode;
 
 import Code.DoublyLinkedList;
-import Code.SinglyLinkedList;
 
 /**
  * This is the interface of the class that finds the essential
  * prime implicants.
- * @author A2HP11
+ * @author H
  *
  */
 public interface IEssentialPrimeImplicants {
 
-	
+	/**
+	 * This method gets the array of MTs covered by given PIs.
+	 * @param primes array of PIs lists.
+	 * @return an array of lists of MTs covered by the PIs.
+	 * Indices of this array correspond to the indices of
+	 * the given PIs array.
+	 */
+	public DoublyLinkedList[] coveredMinterms(DoublyLinkedList[] primes);
+
+	/**
+	 * This method produces a list of given prime implicants.
+	 * @param primes this is the list of prime implicants.
+	 * @param coveredMT this is the array of lists of covered MTs by each PI.
+	 * @return
+	 */
+	public DoublyLinkedList[] coveringImplicants(DoublyLinkedList[] coveredMT);
 }
